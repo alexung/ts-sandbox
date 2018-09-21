cfd7189342297bc74dab531e8d36b4157cbb7dd1
Alex Ung <aung@lyft.com>
2018-09-21 09:59:33 -0700

    typescript config in the tslint to not need I before interface names and got a typed App

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,28 @@ import './App.css';
 
 import logo from './logo.svg';
 
-class App extends React.Component {
+export interface AppProps {
+  instructions: string;
+  welcomeMessage: string;
+}
+
+// AppProps is the interface
+// {} afterward is the state
+class App extends React.Component<AppProps, {}> {
+  public static defaultProps: Partial<AppProps> = {
+    instructions: 'Default Props works!',
+    welcomeMessage: 'Sup',
+  };
+
   public render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">{this.props.welcomeMessage}</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
+          {this.props.instructions}
         </p>
       </div>
     );
