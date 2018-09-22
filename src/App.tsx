67eddbf1a0095dd5b4703e47150bf82d5e30221b
Alex Ung <aung@lyft.com>
2018-09-22 14:28:40 -0700

    got mapbox working

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import * as React from 'react';
 
 import DeckGLOverlay from './components/DeckGLOverlay/index';
-import logo from './logo.svg';
 
 import './App.css';
 
-export interface AppProps {
-  instructions: string;
-  welcomeMessage: string;
-}
+// export interface AppProps {
+//   instructions: string;
+//   welcomeMessage: string;
+// }
 
 export const INITIAL_VIEW_STATE: object = {
   bearing: -27.396674584323023,
@@ -22,30 +21,18 @@ export const INITIAL_VIEW_STATE: object = {
 
 // AppProps is the interface
 // {} afterward is the state
-class App extends React.Component<AppProps, {}> {
-  public static defaultProps: Partial<AppProps> = {
-    instructions: 'Default Props works!',
-    welcomeMessage: 'Sup',
-  };
+// class App extends React.Component<AppProps, {}> {
+class App extends React.Component {
 
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">{this.props.welcomeMessage}</h1>
-        </header>
-        <p className="App-intro">
-          {this.props.instructions}
-        </p>
-        <DeckGLOverlay
-          controller={true}
-          width={window.innerWidth}
-          height={window.innerHeight}
-          mapStyle={"mapbox://styles/mapbox/dark-v9"}
-          preventStyleDiffing={true}
-          initialViewState={INITIAL_VIEW_STATE} />
-      </div>
+      <DeckGLOverlay
+        controller={true}
+        width={window.innerWidth}
+        height={window.innerHeight}
+        mapStyle={"mapbox://styles/mapbox/dark-v9"}
+        preventStyleDiffing={true}
+        initialViewState={INITIAL_VIEW_STATE} />
     );
   }
 }
